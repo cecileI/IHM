d06eae4e915f8a06b2efc83bc1183db1ae9470d1
clarisse <clarisse.dupin@hotmail.fr>
2018-03-07 16:29:46 +0100

    Jtree

--- src/ihm/view/View.java
+++ src/ihm/view/View.java
@@ -16,9 +16,11 @@ import javax.swing.tree.TreeSelectionModel;
  * @author clarisse
  */
 public class View extends JFrame{
-    
+    private JTextField txtNombreEleves,txtPrenom,txtNom,txtAge,txtSexe,txtNiveau,txtProf;
     private JTree tree;
     private DefaultMutableTreeNode racine;
+    private Eleve currentEleve;
+    private Classe currentClasse;
     
     public View(){
     // Tree gestion
@@ -49,6 +51,59 @@ public class View extends JFrame{
                 nodeClasse.add(nodeEleve);
             }
         } 
+    }
+    
+    /*
+    * Cette fonction est appelée apres avoir cliqué sur un élément dans le JTree
+    * La fenetre est mise a jour en fonction de l'élément choisi (Eleve ou Classe)
+    * @param selected Node : l'élément choisi dans l'arbre
+    */
+    public void controllerJTreeCall(Object selectedNode){
+        if (selectedNode instanceof Eleve) { //si l'élément choisi est un élève
+            currentEleve = (Eleve)selectedNode; //l'élève courant est celui sélectionné
+            currentClasse = (Classe) currentEleve.getClasse(); //la classe courante est celle de l'élève sélectionné
+            update();//mis à jour
+            
+        }else if(selectedNode instanceof Classe){ //si l'élément choisi est une classe
+            currentEleve = null; //il n'y a pas d'élève courant
+            currentClasse = (Classe)selectedNode; // la classe courante est celle sélectionnée
+            update(); //mis à jour
+            
+        }else{
+            System.out.println("Erreur");
+        }
+    }
+    
+    /*
+    *Après avoir cliqué sur un élément (ex : dans le JTree)
+    *Fonction qui permet la mise a jour de l'interface
+    */
+    public void update(){
+        if (currentEleve != null) {
+            txtSexe.setText(currentEleve.getSexe()); // Sexe de l'eleve
+            txtAge.setText(currentEleve.getAge()); // Age de l'eleve
+            txtNom.setText(currentEleve.getNom()); // Nom de l'eleve
+            txtPrenom.setText(currentEleve.getPrenom()); //Prenom de l'eleve
+            String taille = "" + (currentEleve.getClasse().getNombreEleves() + ""); //Calcul de la taille de la classe
+            txtNombreEleves.setText(taille); //Affichage du nombre d'eleves dans la classe
+            txtProf.setText(currentEleve.getClasse().getProf()); //Affichage du nom du prof
+            txtNiveau.setText("" + currentEleve.getClasse().getNiveau()); // Affichage du niveau de la classe
+            this.viewTable.setData(currentEleve.getClasse()); //mise a jour JTable
+            
+        }else if(currentClass!=null){
+            String taille = "" + (currentClass.getNombreEleves()); //Calcul de la taille de la classe
+            txtNombreEleves.setText(taille); //Affichage du nombre d'eleves dans la classe
+            txtProf.setText(currentClass.getProf()); //Affichage du nom du prof
+            txtNiveau.setText("" + currentClass.getNiveau()); // Affichage du niveau de la classe
+            this.viewTable.setData(currentClass); //mise a jour JTable
+            txtSexe.setText(""); //Vide le champ sexe
+            txtAge.setText(""); //Vide le champ age
+            txtNom.setText(""); //Vide le champ nom
+            txtPrenom.setText(""); //Vide le champ prenom
+            
+        }else{
+            System.out.print("Erreur");
+        }
     }
     
 }
