f1456cf646c9ed6b6c186ba2e6734553eacd0696
DianeDoum <d.doumenge@yahoo.fr>
2018-03-10 21:59:14 +0100

    Ajout de la liste Tentative Non-Evaluées dans infoBDD

---
+++ src/LogoMain.java
@@ -0,0 +1,26 @@
+import ihm.view.InterfaceDebut;
+import ihm.view.View;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * The class to lanch the application
+ * @author Group7
+ */
+public class LogoMain{
+    
+    /**
+     * The constructor of the class LogoMain
+     * @param args The user do not have to put argument. This parameter is for 
+     * user with command line version of the application.
+     */
+    public static void main (String[] args){
+        
+        InterfaceDebut app = new InterfaceDebut();
+        
+    }
+}
