1e526baad8871727c5eef40fef495c1787b6da29
clarisse <clarisse.dupin@hotmail.fr>
2018-03-13 17:20:18 +0100

    Merge origin/master

--- src/ihm/view/MenuProfesseur.java
+++ src/ihm/view/MenuProfesseur.java
@@ -11,40 +11,37 @@ import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTree;
 import javax.swing.SwingConstants;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeSelectionModel;
 
 /**
- *
+ *  Cette classe permet de créé une fenetre qui permettra a l'élève d'acceder à ces fonctionnalitées
  * @author Goup7
  */
 public class MenuProfesseur extends JFrame {
    
-    private JPanel General;
-    private JPanel Droite;
-    private JPanel PanelHaut;
-    private JPanel PanelBas;
-  
-    private JPanel Tree; 
-    private JLabel Arbre;
-  
-    private JPanel Exercice; 
-    private JButton CreerExercice;
-    private JButton ModifierExercice;
-
-    private JButton deconnection;
+    //JPanel
+    private JPanel panGeneral; //panel général qui regroupe toutes les informations de la Jframe
+    private JPanel panDroite; //panel à droite qui sera modifier par des nouveaux panel
+    private JPanel panHaut; //panel de deconnection et panel blanc
+    private JPanel panExercice; //panel qui regroupe les boutons création et modification exercice
+    
+    //JButton
+    private JButton creerExercice; //bouton création exercice
+    private JButton modifierExercice; //bouton modification exercice
+    private JButton deconnection; //bouton deconnection
+    
+    //JTree
+    private ViewJTree panTree; //panel à gauche Jtree
            
     public MenuProfesseur(){
-        this.getContentPane().setLayout(new BorderLayout()); 
-        this.setTitle("Menu Professeur");
-        this.setSize(750,500);  //largeur, hauteur
-               
+              
         //Partie JTree à gauche 
-        Tree = new JPanel();
-        Tree.setPreferredSize(new Dimension(140,450));
-        Arbre = new JLabel("Affichage du JTree");
-        //Arbre.setVerticalAlignment(SwingConstants.CENTER);
-        //Arbre.setFont(new Font("Arial",Font.BOLD,15));
-        Tree.add(Arbre);         
+            panTree=new ViewJTree();
+            panTree.createTree();
+            //panTree.setPreferredSize(new Dimension(140,450));
                        
         //Boutton Deconnection
         deconnection = new JButton("Deconnection");
@@ -57,58 +54,78 @@ public class MenuProfesseur extends JFrame {
             }); 
         
 
-        //Partie Exercice
-        Exercice = new JPanel();
-        Exercice.setPreferredSize(new Dimension(550,400));
-        Exercice.setLayout(new GridLayout(2,1));
-                      
-        CreerExercice = new JButton("Créer un Exercice");
-        CreerExercice.addActionListener(new ActionListener () {
+        //Partie Exercice     
+            //Bouton création exercice
+        creerExercice = new JButton("Créer un Exercice");
+        creerExercice.addActionListener(new ActionListener () {
                 public void actionPerformed (ActionEvent e) {
                     //recupere les donnees de l'eleve
                     AjoutExerciceProf app = new AjoutExerciceProf();
                 }
             }); 
-              
-        ModifierExercice = new JButton("Modifier un Exercice");
+            //Bouton modification exercice
+        modifierExercice = new JButton("Modifier un Exercice");
         //ModifierExercice.setHorizontalAlignment(SwingConstants.CENTER);
-        ModifierExercice.addActionListener(new ActionListener () {
+        modifierExercice.addActionListener(new ActionListener () {
                 public void actionPerformed (ActionEvent e) {
                     //recupere les donnees de l'eleve
                     ListeExercicesProf panel = new ListeExercicesProf();
                     
                 }
-            }); 
-        
-        Exercice.add(CreerExercice);
-        Exercice.add(ModifierExercice);
-       
-        //Partie Gauche  = Jtree        
-        //Partie Droite de la frame = Haut et Bas
-        Droite = new JPanel();
-        Droite.setPreferredSize(new Dimension(550,450));
-        PanelBas = new JPanel();
-        PanelBas.setPreferredSize(new Dimension(550,400));
-        PanelHaut = new JPanel();
-        PanelHaut.setPreferredSize(new Dimension(550,50));  
-        PanelHaut.setLayout(new GridLayout(1,2));
-                
+            });      
+        // En haut : un blanc et bouton deconnection      
         JLabel labelBlanc = new JLabel("");
         labelBlanc.setHorizontalAlignment(SwingConstants.CENTER);
-        PanelHaut.add(labelBlanc);
-        PanelHaut.add(deconnection);              
-        PanelBas.add(Exercice);
         
-        Droite.add(PanelHaut);
-        Droite.add(PanelBas);
-                
-        //Partie Général
-        General = new JPanel();
-        General.setPreferredSize(new Dimension(750,500));
-        General.add(Tree,BorderLayout.WEST);
-        General.add(Droite,BorderLayout.EAST);
+        panHaut = new JPanel();
+        panHaut.setPreferredSize(new Dimension(550,50));
+        panHaut.setLayout(new GridLayout(1,2));
+        panHaut.add(labelBlanc); //JLabel blanc
+        panHaut.add(deconnection);   //JPanel Deconnection   
         
-        this.add(General);
+        //panExercice qui contient les boutons créer et modifier
+        JLabel blanc1 = new JLabel("");
+        blanc1.setHorizontalAlignment(SwingConstants.CENTER);
+        JLabel blanc2 = new JLabel("");
+        blanc2.setHorizontalAlignment(SwingConstants.CENTER);
+        JLabel blanc3 = new JLabel("");
+        blanc3.setHorizontalAlignment(SwingConstants.CENTER);
+        JLabel blanc4 = new JLabel("");
+        blanc4.setHorizontalAlignment(SwingConstants.CENTER);
+        JLabel blanc5 = new JLabel("");
+        blanc5.setHorizontalAlignment(SwingConstants.CENTER);
+        JLabel blanc6 = new JLabel("");
+        blanc6.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        
+        panExercice = new JPanel();
+        panExercice.setPreferredSize(new Dimension(550,400));
+        panExercice.setLayout(new GridLayout(4,2));
+        panExercice.add(blanc1);
+        panExercice.add(blanc2);
+        panExercice.add(creerExercice); //JButton créer exercice
+        panExercice.add(modifierExercice); //JButton modifier exercice
+        panExercice.add(blanc3);
+        panExercice.add(blanc4);
+        panExercice.add(blanc5);
+        panExercice.add(blanc6);
+        
+        //panDroite qui contient panHaut et panExercice
+        panDroite = new JPanel();
+        panDroite.setPreferredSize(new Dimension(550,450));
+        panDroite.add(panHaut);
+        panDroite.add(panExercice);
+        
+        panGeneral = new JPanel();
+        panGeneral.setLayout(new BorderLayout()); 
+        panGeneral.add(panTree,BorderLayout.WEST);     //ajout du JTree au menu (à gauche)
+        panGeneral.add(panDroite,BorderLayout.EAST);   //ajout du panel général au menu
+        
+        this.add(panGeneral);
+        
+        //this.getContentPane().setLayout(new BorderLayout()); 
+        this.setTitle("Menu Professeur");
+        this.setSize(750,500);  //largeur, hauteur
         
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
