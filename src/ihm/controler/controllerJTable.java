cd221f441c59eed99539645f96e48253d50685f3
Kelly <kelly33480@hotmail.fr>
2018-03-21 14:38:14 +0100

    Suppression ControllerJTable

--- src/ihm/controler/controllerJTable.java
+++ src/ihm/controler/controllerJTable.java
@@ -1,9 +1,32 @@
 package ihm.controler;
 
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import ihm.view.*;
+
 /**
- *
- * @author Goup7
+ * Création du controllerJTable
+ * @author Kelly 
  */
-public class controllerJTable {
+public class controllerJTable implements ListSelectionListener{
+    private JTable jtable;
+    private MenuEleve menueleve;
+    private MenuProfesseur menuprof;
+    
+ /**
+ * Constructeur du controllerJTable
+ * @author Kelly 
+ */
+      public controllerTable(JTable jtable, MenuEleve me) {
+      this.jtable = jtable;
+      this.menueleve = me;
+	}
+
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+        
+    }
     
 }
