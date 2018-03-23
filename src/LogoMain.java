8f188d08fb0b6d7ea5ef015420d2dd1d02b856f9
Kelly <kelly33480@hotmail.fr>
2018-03-23 15:01:10 +0100

    Merge origin/master
    Conflicts:
    	src/LogoMain.java

--- src/LogoMain.java
+++ src/LogoMain.java
@@ -27,9 +27,9 @@ public class LogoMain{
         //Classe classeCE1=new Classe("CE1");
         //Classe classeCP=new Classe("CP");
          //InterfaceDebut app = new InterfaceDebut();
-         //MenuEleve app = new MenuEleve();
+         MenuEleve app = new MenuEleve();
 
-        InterfaceDebut app = new InterfaceDebut();
+        //InterfaceDebut app = new InterfaceDebut();
         //MenuEleve app = new MenuEleve();
         //MenuProfesseur app = new MenuProfesseur();
         //AjoutExerciceProf app = new AjoutExerciceProf();
